29d24fa6fb355d02edfedb64391b8ee9e4193e80
bigrocs <bigrocs@qq.com>
2020-12-07 09:05:04 +0800

    自动提交 git 代码

--- srv/usb.h
+++ srv/usb.h
@@ -4,6 +4,7 @@
 #include <initguid.h>
 #include <setupapi.h>
 #include <usbiodef.h>
-#pragma comment(lib, "setupapi.lib")
+#define LIBRARY_NAME ("setupapi.dll")
+// #pragma comment(lib, "setupapi.lib")
 
 Napi::Object GetUsbDeviceList(const Napi::CallbackInfo &info);
