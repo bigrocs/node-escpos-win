03d592c8a05eded4983d591aa946b55aacd0275a
bigrocs <bigrocs@qq.com>
2020-12-07 09:13:17 +0800

    自动提交 git 代码

--- srv/usb.cc
+++ srv/usb.cc
@@ -22,6 +22,7 @@
 Napi::Object GetUsbDeviceList(const Napi::CallbackInfo &info)
 {
     HDEVINFO hDevInfoSet;
+    SP_DEVINFO_DATA spDevInfoData;
 
     PSP_DEVICE_INTERFACE_DETAIL_DATA pDetail;
     SP_DEVICE_INTERFACE_DATA ifdata;
@@ -51,20 +52,19 @@ Napi::Object GetUsbDeviceList(const Napi::CallbackInfo &info)
     {
         Napi::Object device = Napi::Object::New(env);
         // 枚举符合该GUID的设备接口
-        SP_DEVINFO_DATA* pspDevInfoData = (SP_DEVINFO_DATA *)HeapAlloc(GetProcessHeap(), 0, sizeof(SP_DEVINFO_DATA));
-        pspDevInfoData->cbSize = sizeof(SP_DEVINFO_DATA);
+        spDevInfoData->cbSize = sizeof(SP_DEVINFO_DATA);
         bResult = ::SetupDiEnumDeviceInfo(hDevInfoSet,     // 设备信息集句柄
-                                        (ULONG)nCount,   // 设备信息集里的设备序号
-                                        &pspDevInfoData); // 设备接口信息
+                                          (ULONG)nCount,   // 设备信息集里的设备序号
+                                          &spDevInfoData); // 设备接口信息
         if (bResult){
             DWORD DataT;
             TCHAR buf[MAX_PATH];
             DWORD nSize = 0;
-            if (SetupDiGetDeviceRegistryPropertyA(hDevInfoSet, pspDevInfoData, SPDRP_FRIENDLYNAME, &DataT, (PBYTE)buf, sizeof(buf), &nSize))
+            if (SetupDiGetDeviceRegistryProperty(hDevInfoSet, &spDevInfoData, SPDRP_FRIENDLYNAME, &DataT, (PBYTE)buf, sizeof(buf), &nSize))
             {
                 device.Set(Napi::String::New(env, "name"), buf);
             }
-            else if (SetupDiGetDeviceRegistryPropertyA(hDevInfoSet, pspDevInfoData, SPDRP_DEVICEDESC, &DataT, (PBYTE)buf, sizeof(buf), &nSize))
+            else if (SetupDiGetDeviceRegistryProperty(hDevInfoSet, &spDevInfoData, SPDRP_DEVICEDESC, &DataT, (PBYTE)buf, sizeof(buf), &nSize))
             {
                 device.Set(Napi::String::New(env, "name"), buf);
             }
