fa4f00b9765bb7f859f85bbaf49c5b3dd67f9fce
bigrocs <bigrocs@qq.com>
2020-12-08 13:54:24 +0800

    自动提交 git 代码

--- srv/io.cc
+++ srv/io.cc
@@ -25,7 +25,7 @@ Object Write(const CallbackInfo &info) {
     // char *deviceBf = (char *)malloc(devicePath->Utf8Length() + 1);
 
     // obj.Set(String::New(env, "deviceBf"), deviceBf);
-    obj.Set(String::New(env, "devicePath"), devicePath.Utf8Value());
+    obj.Set(String::New(env, "devicePath"), devicePath.Utf8Value().c_str());
     obj.Set(String::New(env, "data"), data);
     obj.Set(String::New(env, "test"), "Hello Write Object");
 
