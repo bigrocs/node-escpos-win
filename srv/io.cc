cd2041514110a4ac8614edb612a1ea64a3135c8b
bigrocs <bigrocs@qq.com>
2020-12-08 11:07:42 +0800

    自动提交 git 代码

--- srv/io.cc
+++ srv/io.cc
@@ -4,24 +4,24 @@ Object Write(const CallbackInfo &info) {
     Env env = info.Env();
     if (info.Length() < 2)
     {
-        Napi::TypeError::New(env, "Wrong number of arguments, must be 2")
+        TypeError::New(env, "Wrong number of arguments, must be 2")
             .ThrowAsJavaScriptException();
         return env.Null();
     }
 
     if (!info[0].IsString())
     {
-        Napi::TypeError::New(env, "The first argument must be a string").ThrowAsJavaScriptException();
+        TypeError::New(env, "The first argument must be a string").ThrowAsJavaScriptException();
         return env.Null();
     }
 
     if (!info[1].IsObject())
     {
-        Napi::TypeError::New(env, "the second argument must be a buffer").ThrowAsJavaScriptException();
+        TypeError::New(env, "the second argument must be a buffer").ThrowAsJavaScriptException();
         return env.Null();
     }
-    String arg0 = info[0].As<Napi::String>().Utf8Value();
-    ArrayBuffer arg1 = info[1].As<Napi::ArrayBuffer>();
+    String arg0 = info[0].As<String>().Utf8Value();
+    ArrayBuffer arg1 = info[1].As<ArrayBuffer>();
 
     Object obj = Object::New(env); // 初始化函数返回数据对象
     obj.Set(String::New(env, "arg0"), arg0);
