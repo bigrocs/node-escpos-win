0ddc00dd03287a4d97306197594a11e4ef35ebea
bigrocs <bigrocs@qq.com>
2020-12-08 11:06:24 +0800

    自动提交 git 代码

--- srv/io.cc
+++ srv/io.cc
@@ -2,7 +2,30 @@
 
 Object Write(const CallbackInfo &info) {
     Env env = info.Env();
+    if (info.Length() < 2)
+    {
+        Napi::TypeError::New(env, "Wrong number of arguments, must be 2")
+            .ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    if (!info[0].IsString())
+    {
+        Napi::TypeError::New(env, "The first argument must be a string").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    if (!info[1].IsObject())
+    {
+        Napi::TypeError::New(env, "the second argument must be a buffer").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+    String arg0 = info[0].As<Napi::String>().Utf8Value();
+    ArrayBuffer arg1 = info[1].As<Napi::ArrayBuffer>();
+
     Object obj = Object::New(env); // 初始化函数返回数据对象
+    obj.Set(String::New(env, "arg0"), arg0);
+    obj.Set(String::New(env, "arg1"), arg1);
     obj.Set(String::New(env, "test"), "Hello Write Object");
     return obj;
 }
