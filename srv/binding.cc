9523c50d529cc5c82db76ffaa25308c2f9a95784
bigrocs <bigrocs@qq.com>
2020-12-08 16:22:02 +0800

    自动提交 git 代码

--- srv/binding.cc
+++ srv/binding.cc
@@ -1,5 +1,5 @@
 #include <napi.h>
-// #include "usb.h"
+#include "usb.h"
 // #include "io.h"
 
 using namespace Napi;
@@ -9,7 +9,7 @@ String Hello(const CallbackInfo& info) {
   return String::New(env, "Hello World");
 }
 
-Object GetUsbDeviceList(const CallbackInfo &info)
+Object UsbDeviceList(const CallbackInfo &info)
 {
   Env env = info.Env();
   Object obj = Object::New(env); // 初始化函数返回数据对象
@@ -23,7 +23,7 @@ Object Init(Env env, Object exports) {
   exports.Set(String::New(env, "hello"),
               Function::New(env, Hello));
   exports.Set(String::New(env, "GetUsbDeviceList"),
-              Function::New(env, GetUsbDeviceList));
+              Function::New(env, UsbDeviceList));
   // exports.Set(String::New(env, "Write"),
   //             Function::New(env, Write));
   return exports;
