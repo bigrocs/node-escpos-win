9e47aa2b70a383ba38664d51e4fbe09dc2ec78b7
bigrocs <bigrocs@qq.com>
2020-12-08 10:41:20 +0800

    自动提交 git 代码

--- srv/io.cc
+++ srv/io.cc
@@ -4,6 +4,6 @@ Object Write(const CallbackInfo &info)
 {
     Env env = info.Env();
     Object obj = Object::New(env); // 初始化函数返回数据对象
-    obj.Set(String::New(env, "number"), "Hello Write Object");
+    obj.Set(String::New(env, "test"), "Hello Write Object");
     return obj;
 }
