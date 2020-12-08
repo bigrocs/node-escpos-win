6b04345d7b49e97e4506e75253a66e6416096576
bigrocs <bigrocs@qq.com>
2020-12-08 15:58:46 +0800

    自动提交 git 代码

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var addon = require('bindings')('addon');
 
-const iconv = require('iconv-lite');
-const usb = addon.GetUsbDeviceList();
-const printer = usb.list.find(item => item.service === 'usbprint' || item.name === 'USB 打印支持');
-const content = iconv.encode("你好啊\n halo cpp!\\n\n\n\n\nn\n\n\n\n", 'GB18030');
-const res = addon.Write(printer.path, content);
-console.log(content, printer.path, res); // 'world'
+// const iconv = require('iconv-lite');
+// const usb = addon.GetUsbDeviceList();
+// const printer = usb.list.find(item => item.service === 'usbprint' || item.name === 'USB 打印支持');
+// const content = iconv.encode("你好啊\n halo cpp!\\n\n\n\n\nn\n\n\n\n", 'GB18030');
+// const res = addon.Write(printer.path, content);
+// console.log(content, printer.path, res); // 'world'
 
+console.log(addon.GetUsbDeviceList());
 
 
 const win32Escpos = function () {
