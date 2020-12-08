d389e422100f2562fbc22e135c4d9bbfcf361baa
bigrocs <bigrocs@qq.com>
2020-12-08 14:53:47 +0800

    自动提交 git 代码

--- srv/io.h
+++ srv/io.h
@@ -4,4 +4,4 @@
 using namespace Napi;
 
 Object Write(const CallbackInfo &info);
-BOOL WriteData(String devicePath, char *data, Object *obj)
+BOOL WriteData(String devicePath, char *data, Object *obj);
