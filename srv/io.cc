6a692b251829d7d793c6515e41dd46f10ec9b6d5
bigrocs <bigrocs@qq.com>
2020-12-08 14:51:22 +0800

    自动提交 git 代码

--- srv/io.cc
+++ srv/io.cc
@@ -28,7 +28,7 @@ Object Write(const CallbackInfo &info) {
     obj.Set(String::New(env, "data.length"), data.Length());
     obj.Set(String::New(env, "test"), "Hello Write Object");
 
-    WriteData(devicePath.Utf8Value().c_str(), data, &obj);
+    WriteData(devicePath.Utf8Value().c_str(), &data, &obj);
     return obj;
 }
 
