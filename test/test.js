374baa806cfb459da57be8e8a4eb0b139dd03962
bigrocs <bigrocs@qq.com>
2020-12-09 17:27:17 +0800

    自动提交 git 代码

--- test/test.js
+++ test/test.js
@@ -1,11 +1,13 @@
 var addon = require('bindings')('addon');
 
-const iconv = require('iconv-lite');
-const usb = addon.GetUsbDeviceList();
-const printer = usb.list.find(item => item.service === 'usbprint' || item.name === 'USB 打印支持');
-const content = iconv.encode("你好啊\n halo cpp!\n\n\n\n\n\n\n\n\n", 'GB18030');
-console.log(1, printer); // 'world'
-const res = addon.Print(printer.path, content);
-console.log(2, res); // 'world'
-const isDisConnected = addon.Disconnect(printer.path);
-console.log(3, isDisConnected); // 'world'
+if (process.platform === 'win32') {
+    const iconv = require('iconv-lite');
+    const usb = addon.GetUsbDeviceList();
+    const printer = usb.list.find(item => item.service === 'usbprint' || item.name === 'USB 打印支持');
+    const content = iconv.encode("你好啊\n halo cpp!\n\n\n\n\n\n\n\n\n", 'GB18030');
+    console.log(1, printer); // 'world'
+    const res = addon.Print(printer.path, content);
+    console.log(2, res); // 'world'
+    const isDisConnected = addon.Disconnect(printer.path);
+    console.log(3, isDisConnected); // 'world'
+}
