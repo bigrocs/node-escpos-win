785c89af1cd0f811fcf129345121bef502dbbdaf
bigrocs <bigrocs@qq.com>
2020-12-12 20:47:46 +0800

    自动提交 git 代码

--- srv/io.cc
+++ srv/io.cc
@@ -81,7 +81,7 @@ HANDLE InitPort(PrintDevice &device)
     HANDLE handle = CreateFile(device.Port.c_str(), GENERIC_READ | GENERIC_WRITE,
                                0, NULL,
                                OPEN_EXISTING,
-                               FILE_FLAG_OVERLAPPED, NULL);
+                               FILE_ATTRIBUTE_NORMAL, NULL);
 
     if (handle == INVALID_HANDLE_VALUE)
     { // 打开端口失败
