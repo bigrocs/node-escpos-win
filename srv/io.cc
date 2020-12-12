bfd88c0b16920a289c9d0745dcf0e1cccfc02c77
bigrocs <bigrocs@qq.com>
2020-12-12 22:10:29 +0800

    自动提交 git 代码

--- srv/io.cc
+++ srv/io.cc
@@ -81,8 +81,8 @@ HANDLE InitPort(PrintDevice &device)
     HANDLE handle = CreateFile(device.Port.c_str(), GENERIC_READ | GENERIC_WRITE,
                                0, NULL,
                                OPEN_EXISTING,
-                               0, NULL);
-    cout << "last err is " << FILE_ATTRIBUTE_NORMAL << endl;
+                               FILE_FLAG_DELETE_ON_CLOSE, NULL);
+    cout << "last err is " << FILE_FLAG_DELETE_ON_CLOSE << endl;
     if (handle == INVALID_HANDLE_VALUE)
     { // 打开端口失败
         return FALSE;
