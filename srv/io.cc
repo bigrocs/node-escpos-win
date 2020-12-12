d72db4d009250e7623fa400b09c08209d69df060
bigrocs <bigrocs@qq.com>
2020-12-12 22:22:56 +0800

    自动提交 git 代码

--- srv/io.cc
+++ srv/io.cc
@@ -88,45 +88,45 @@ HANDLE InitPort(PrintDevice &device)
     }
     else
     {
-        //设置端口缓冲
-        int a = SetupComm(handle, 1024, 1024);
-        // 设定通讯端口超时参数
-        COMMTIMEOUTS tmouts;
-        tmouts.ReadIntervalTimeout = 100;
-        tmouts.ReadTotalTimeoutMultiplier = 100;
-        tmouts.ReadTotalTimeoutConstant = 100;
-        tmouts.WriteTotalTimeoutConstant = 100;
-        tmouts.WriteTotalTimeoutMultiplier = 100;
-        int b = SetCommTimeouts(handle, &tmouts);
+        // //设置端口缓冲
+        // SetupComm(handle, 1024, 1024);
+        // // 设定通讯端口超时参数
+        // COMMTIMEOUTS tmouts;
+        // tmouts.ReadIntervalTimeout = 100;
+        // tmouts.ReadTotalTimeoutMultiplier = 100;
+        // tmouts.ReadTotalTimeoutConstant = 100;
+        // tmouts.WriteTotalTimeoutConstant = 100;
+        // tmouts.WriteTotalTimeoutMultiplier = 100;
+        // SetCommTimeouts(handle, &tmouts);
 
-        //设定通讯端口通讯参数
-        DCB dcb;
-        BOOL bol = TRUE;
+        // //设定通讯端口通讯参数
+        // DCB dcb;
+        // BOOL bol = TRUE;
 
-        //dcb.DCBlength = sizeof(dcb);
-        bol = GetCommState(handle, &dcb);
-        dcb.BaudRate = device.BawdRate;
-        dcb.ByteSize = device.DataBits;
-        dcb.StopBits = device.StopBits;
-        dcb.Parity = device.Parity;
+        // //dcb.DCBlength = sizeof(dcb);
+        // bol = GetCommState(handle, &dcb);
+        // dcb.BaudRate = device.BawdRate;
+        // dcb.ByteSize = device.DataBits;
+        // dcb.StopBits = device.StopBits;
+        // dcb.Parity = device.Parity;
 
-        bol = SetCommState(handle, &dcb); //配置串口
-                                          // 清除通讯端口缓存
-        PurgeComm(handle, PURGE_TXCLEAR | PURGE_RXCLEAR | PURGE_TXABORT | PURGE_RXABORT);
+        // bol = SetCommState(handle, &dcb); //配置串口
+        //                                   // 清除通讯端口缓存
+        // PurgeComm(handle, PURGE_TXCLEAR | PURGE_RXCLEAR | PURGE_TXABORT | PURGE_RXABORT);
 
-        // 初始化重叠IO对象
-        OVERLAPPED m_OverlappedRead;
-        OVERLAPPED m_OverlappedWrite;
-        HANDLE m_hStopCommEvent;
-        HANDLE m_hDataReady;
-        memset(&m_OverlappedRead, 0, sizeof(OVERLAPPED));
-        m_OverlappedRead.hEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
-        memset(&m_OverlappedWrite, 0, sizeof(OVERLAPPED));
-        m_OverlappedWrite.hEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
+        // // 初始化重叠IO对象
+        // OVERLAPPED m_OverlappedRead;
+        // OVERLAPPED m_OverlappedWrite;
+        // HANDLE m_hStopCommEvent;
+        // HANDLE m_hDataReady;
+        // memset(&m_OverlappedRead, 0, sizeof(OVERLAPPED));
+        // m_OverlappedRead.hEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
+        // memset(&m_OverlappedWrite, 0, sizeof(OVERLAPPED));
+        // m_OverlappedWrite.hEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
 
-        // 初始化事件对象
-        m_hStopCommEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
-        m_hDataReady = CreateEvent(NULL, FALSE, FALSE, NULL);
+        // // 初始化事件对象
+        // m_hStopCommEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+        // m_hDataReady = CreateEvent(NULL, FALSE, FALSE, NULL);
 
         //初始化打印ESC @
         // DWORD iBytesLength;
