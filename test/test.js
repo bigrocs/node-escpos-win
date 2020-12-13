900ecdffcd5ddcfe51e47fd204a8f81a4f0a1aa1
bigrocs <bigrocs@qq.com>
2020-12-13 17:27:26 +0800

    自动提交 git 代码

--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ var addon = require('bindings')('addon');
         const printer = usb.list.find(item => item.service === 'usbprint' || item.name === 'USB 打印支持');
         const content = iconv.encode("bigrocs!", 'GB18030');
         console.log(1, printer); // 'world'
-        const res = addon.Print("COM", content);
+        const res = addon.Print("LPT1", content);
         console.log(2, res); // 'world'
         const isDisConnected = addon.Disconnect(printer.path);
         console.log(3, isDisConnected); // 'world'
