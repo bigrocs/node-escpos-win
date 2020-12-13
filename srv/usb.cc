8b992e3e45a1fe7cbdfd68e6858010d7fa885bc3
bigrocs <bigrocs@qq.com>
2020-12-13 18:00:52 +0800

    自动提交 git 代码

--- srv/usb.cc
+++ srv/usb.cc
@@ -107,10 +107,10 @@ void GetDeviceList(list<DeviceInfo> &devicelist, string deviceType, ResultInfo &
             if (bResult)
             {
                 deviceinfo.path = pDetail->DevicePath;
+                nCount++; // 调整计数值
+                devicelist.push_back(deviceinfo);
             }
         }
-        nCount++; // 调整计数值
-        devicelist.push_back(deviceinfo);
     }
     resultInfo.success = true;
     ::GlobalFree(pDetail);                       // 释放设备接口数据空间
